23c96c963e06e297c2cabfe1d513f282e3193b8a
Chris Ortman <chris-ortman@uiowa.edu>
2015-02-20 10:57:24 -0600

    Import of code from uiris3

---
+++ lib/acts_as_state_machine/version.rb
@@ -0,0 +1,3 @@
+module ActsAsStateMachine
+  VERSION = "0.0.1"
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '5d4c5d71f596161b18482c9f86fcb8a38da51836996a4f5d635af8f52edb2cadc7a3e032fb505517bfdb734b0f032f77ca5d1a749383be8f29395e67853efcce'
